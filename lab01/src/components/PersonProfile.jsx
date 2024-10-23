d8d06f07b38012151d675c736b19cb6daa9d5f73
Oleh Kniaziev <oleh.kniaziev@microsoft.wsei.edu.pl>
2024-10-23 08:35:18 +0200

    Added styling to the `PersonProfile` component

--- lab01/src/components/PersonProfile.jsx
+++ lab01/src/components/PersonProfile.jsx
@@ -1,11 +1,18 @@
 function PersonProfile({ id, name, eyes }) {
-    return (
-        <div>
-            <p>Id: {id}</p>
-            <p>Name: {name}</p>
-            <p>Eye color: {eyes}</p>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        border: "2px solid black",
+        width: "fit-content",
+        margin: "4px auto",
+        padding: "2px",
+      }}
+    >
+      <p>Id: {id}</p>
+      <p>Name: {name}</p>
+      <p>Eye color: {eyes}</p>
+    </div>
+  );
 }
 
 export default PersonProfile;
